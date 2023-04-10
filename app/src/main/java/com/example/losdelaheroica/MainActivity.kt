9f503016132a9185674abbaab8d59cc4a4052c2c
mora77 <bymora77@gmail.com>
2023-04-10 15:02:50 -0600

    My first commit

--- app/src/main/java/com/example/losdelaheroica/MainActivity.kt
+++ app/src/main/java/com/example/losdelaheroica/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             LosDeLaHeroicaTheme {
                 // A surface container using the 'background' color from the theme
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
