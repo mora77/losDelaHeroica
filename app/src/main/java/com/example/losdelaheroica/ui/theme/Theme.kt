c1d753b395a87c78657dd283cdfcca54c8b885dc
mora77 <bymora77@gmail.com>
2023-04-10 15:01:42 -0600

    My first commit

--- app/src/main/java/com/example/losdelaheroica/ui/theme/Theme.kt
+++ app/src/main/java/com/example/losdelaheroica/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LosDeLaHeroicaTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun LosDeLaHeroicaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
